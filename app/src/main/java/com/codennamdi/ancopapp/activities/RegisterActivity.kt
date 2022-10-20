67d5a8f6ca186b7655c8bf360c078ccfa391e42d
Nnamdi600000 <63124115+Nnamdi600000@users.noreply.github.com>
2022-10-20 08:06:13 -0700

    Added the validateForm, registerUser function in register activity

--- app/src/main/java/com/codennamdi/ancopapp/activities/RegisterActivity.kt
+++ app/src/main/java/com/codennamdi/ancopapp/activities/RegisterActivity.kt
@@ -2,7 +2,8 @@ package com.codennamdi.ancopapp.activities
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.text.TextUtils
+import android.widget.Toast
 import com.codennamdi.ancopapp.R
 import com.codennamdi.ancopapp.databinding.ActivityRegisterBinding
 
@@ -24,7 +25,42 @@ class RegisterActivity : BaseActivity() {
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        binding.btnRegisterActivityRegister.setOnClickListener {
+            registerUser()
+        }
     }
 
+    private fun validateForm(fullName: String, email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(fullName) -> {
+                showErrorSnackBar("Please enter a name")
+                return false
+            }
 
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter a name")
+                return false
+            }
+
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter a name")
+                return false
+            }
+
+            else -> {
+                true
+            }
+        }
+    }
+
+    private fun registerUser() {
+        val fullName = binding.textFieldFullName.text.toString().trim { it <= ' ' }
+        val email = binding.textFieldEmail.text.toString().trim { it <= ' ' }
+        val password = binding.textFieldPassword.text.toString().trim { it <= ' ' }
+
+        if (validateForm(fullName, email, password)) {
+            Toast.makeText(this@RegisterActivity, "Registered!", Toast.LENGTH_LONG).show()
+        }
+    }
 }
