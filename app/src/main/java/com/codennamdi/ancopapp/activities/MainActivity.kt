5d9447ec04a8c267be58ae5d6ce108639c539e80
Nnamdi600000 <63124115+Nnamdi600000@users.noreply.github.com>
2022-10-23 13:05:43 -0700

    Implemented the functionality for the bottom nav & implemented the toolbar

--- app/src/main/java/com/codennamdi/ancopapp/activities/MainActivity.kt
+++ app/src/main/java/com/codennamdi/ancopapp/activities/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.codennamdi.ancopapp.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import com.codennamdi.ancopapp.R
 import com.codennamdi.ancopapp.databinding.ActivityMainBinding
+import com.codennamdi.ancopapp.fragments.ChapelineFragment
+import com.codennamdi.ancopapp.fragments.HomeFragment
+import com.codennamdi.ancopapp.fragments.HymnFrament
+import com.codennamdi.ancopapp.fragments.LeadersFragment
+import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +22,89 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.bottomNav.setOnItemSelectedListener(this)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        (R.id.profile) -> {
+            Toast.makeText(this, "Clicked Profile", Toast.LENGTH_LONG).show()
+            true
+        }
+
+        (R.id.settings) -> {
+            Toast.makeText(this, "Clicked Settings", Toast.LENGTH_LONG).show()
+            true
+        }
+
+        (R.id.sign_out) -> {
+            Toast.makeText(this, "Clicked Sign out", Toast.LENGTH_LONG).show()
+            true
+        }
+
+        else -> super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem) = when (item.itemId) {
+        (R.id.home_menu_id) -> {
+            clickedHomeNavBtn()
+        }
+
+        (R.id.Hymns) -> {
+            clickedHymnsNavBtn()
+        }
+
+        (R.id.leaders) -> {
+            clickedLeadersNavBtn()
+        }
+
+        (R.id.library) -> {
+            clickedLibraryNavBtn()
+        }
+
+        (R.id.chapeline_bio) -> {
+            clickedChapelineBioNavBtn()
+        }
+
+        else -> false
+    }
+
+    private fun clickedHomeNavBtn(): Boolean {
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container_view_id, HomeFragment())
+        }
+        return true
+    }
+
+    private fun clickedHymnsNavBtn(): Boolean {
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container_view_id, HymnFrament())
+        }
+        return true
+    }
+
+    private fun clickedLeadersNavBtn(): Boolean {
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container_view_id, LeadersFragment())
+        }
+        return true
+    }
+
+    private fun clickedLibraryNavBtn(): Boolean {
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container_view_id, HymnFrament())
+        }
+        return true
+    }
+
+    private fun clickedChapelineBioNavBtn(): Boolean {
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container_view_id, ChapelineFragment())
+        }
+        return true
     }
 }
--- app/src/main/java/com/codennamdi/ancopapp/fragments/HymnFrament.kt
+++ app/src/main/java/com/codennamdi/ancopapp/fragments/HymnFrament.kt
@@ -7,14 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.codennamdi.ancopapp.R
 
-class HomeFragment : Fragment() {
+class HymnFrament : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_hymn, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
