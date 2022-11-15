757c420177b941c2ffe9f3220ecc51bb995a2368
Nnamdi600000 <63124115+Nnamdi600000@users.noreply.github.com>
2022-11-15 05:03:07 -0800

    added the home, leaders, library fragments, also added some icons, added the bottom nav menu item, also changed the theme colour all in the admin app

--- ancop_admin/src/main/java/com/codennamdi/ancop_admin/MainActivity.kt
+++ ancop_admin/src/main/java/com/codennamdi/ancop_admin/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.codennamdi.ancop_admin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import com.codennamdi.ancop_admin.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        title = getString(R.string.add_event)
+        binding.bottomNavAdmin.setOnItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem) = when (item.itemId) {
+        (R.id.home_menu_admin_id) -> {
+            clickedAdminHomeNavBtn()
+        }
+
+        (R.id.leaders_menu_admin_id) -> {
+            clickedAdminLeadersNavBtn()
+        }
+
+        (R.id.library_menu_admin_id) -> {
+            clickedAdminLibraryNavBtn()
+        }
+        else -> false
+    }
+
+    private fun clickedAdminHomeNavBtn(): Boolean {
+        supportFragmentManager.commit {
+            replace(R.id.admin_fragment_container_view_id, HomeFragmentAdmin())
+        }
+        title = getString(R.string.add_event)
+        return true
+    }
+
+    private fun clickedAdminLeadersNavBtn(): Boolean {
+        supportFragmentManager.commit {
+            replace(R.id.admin_fragment_container_view_id, LeadersFragmentAdmin())
+        }
+        title = getString(R.string.add_leaders)
+        return true
+    }
+
+    private fun clickedAdminLibraryNavBtn(): Boolean {
+        supportFragmentManager.commit {
+            replace(R.id.admin_fragment_container_view_id, LibraryFragmentAdmin())
+        }
+        title = getString(R.string.add_books)
+        return true
     }
 }
