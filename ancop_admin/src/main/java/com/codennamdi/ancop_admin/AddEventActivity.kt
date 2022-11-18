ecd3f417d8321b56f963daed6eabb6b9874c4dce
Nnamdi600000 <63124115+Nnamdi600000@users.noreply.github.com>
2022-11-18 02:20:54 -0800

    Implemented the picking of image from gallery and permissions

--- ancop_admin/src/main/java/com/codennamdi/ancop_admin/AddEventActivity.kt
+++ ancop_admin/src/main/java/com/codennamdi/ancop_admin/AddEventActivity.kt
@@ -1,11 +1,100 @@
 package com.codennamdi.ancop_admin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.codennamdi.ancop_admin.databinding.ActivityAddEventBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.io.IOException
 
 class AddEventActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddEventBinding
+    private var profileImageResultData: Uri? = null
+
+    private val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK && result.data != null) {
+                profileImageResultData = result.data?.data!!
+                Log.e("Saved image", "$profileImageResultData")
+                try {
+                    Glide
+                        .with(this@AddEventActivity)
+                        .load(profileImageResultData)
+                        .centerCrop()
+                        .placeholder(R.drawable.profile_place_holder)
+                        .into(binding.eventImageImageViewId)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_event)
+        binding = ActivityAddEventBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addEventImageBtn.setOnClickListener {
+            choosePhotoFromGallery()
+        }
+    }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withContext(this@AddEventActivity)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report!!.areAllPermissionsGranted()) {
+                        val pickIntent =
+                            Intent(
+                                Intent.ACTION_PICK,
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                            )
+                        openGalleryLauncher.launch(pickIntent)
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>,
+                    token: PermissionToken
+                ) {
+                    displayRationalDialog()
+                }
+            }).onSameThread().check()
+    }
+
+    private fun displayRationalDialog() {
+        AlertDialog.Builder(this@AddEventActivity)
+            .setMessage("You can't access the gallery because you did not enable the permission, You can do that by heading to the app settings.")
+
+            .setPositiveButton("GOTO SETTINGS") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 }
